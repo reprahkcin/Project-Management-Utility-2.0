c0924343148c728d7d4bccefac3534bd34b9c127
NickHarper <bignickharper@gmail.com>
2019-04-04 06:35:35 -0700

    Trying to put project class together

---
+++ Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Management_Utility_2._0
+{
+    class Project
+    {
+        private string _id;
+        public string projectStatus { get; set; }
+        public string projectType { get; set; }
+        public string department { get; set; }
+        public string courseNumber { get; set; }
+        public string term { get; set; }
+        public string projectName { get; set; }
+        public string projectDescription { get; set; }
+        public string learningObjective { get; set; }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+    }
+}
---
+++ ProjectCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Management_Utility_2._0
+{
+    class ProjectCollection
+    {
+        private List<Project> projects;
+
+        public List<Project> Projects
+        {
+            get { return projects; }
+            set { projects = value; }
+        }
+    }
+}
